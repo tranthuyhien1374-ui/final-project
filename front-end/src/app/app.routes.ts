442a4898e183273063da2785bc4e09aae3567c74
unknown <hieuvo28304@gmail.com>
2025-03-18 20:25:41 +0700

    Them trang chu

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TrangChuComponent } from './trang-chu/trang-chu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/trang-chu', pathMatch: 'full' },
+  { path: 'trang-chu', component: TrangChuComponent }
+];
