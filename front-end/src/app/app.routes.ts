e808606ce3c69ac86c939756a40795132c357dcd
Tran Thuy Hien <hientt22411@st.uel.edu.vn>
2025-03-20 01:48:16 +0700

    [fix] xử lý conflic theo dõi đơn và blog

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { TuTruyenComponent } from './tu-truyen/tu-truyen.component';
 import { TatCaTruyenComponent } from './tat-ca-truyen/tat-ca-truyen.component';
 import { TatCaBlogComponent } from './tat-ca-blog/tat-ca-blog.component';
 import { ChiTietBlogComponent } from './chi-tiet-blog/chi-tiet-blog.component';
-
+import { TheoDoiDonHangComponent } from './theo-doi-don-hang/theo-doi-don-hang.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/trang-chu', pathMatch: 'full' },
   { path: 'trang-chu', component: TrangChuComponent },
   { path: 'tu-truyen', component: TuTruyenComponent },
   { path: 'tat-ca-truyen', component: TatCaTruyenComponent },
   {path: 'tat-ca-blog', component: TatCaBlogComponent},
-  {path: 'chi-tiet-blog', component: ChiTietBlogComponent}
+  {path: 'chi-tiet-blog', component: ChiTietBlogComponent},
+  { path: 'theo-doi-don-hang', component: TheoDoiDonHangComponent }
 ];
