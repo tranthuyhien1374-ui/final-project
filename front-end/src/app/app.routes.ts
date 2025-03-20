364b0572fcc4f8eeabdec3f164e8ed6483d44a12
LeDinhHuy <huyld22411@st.uel.edu.vn>
2025-03-20 16:04:10 +0700

    [Feat]tacgia

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { TatCaTruyenComponent } from './tat-ca-truyen/tat-ca-truyen.component';
 import { TatCaBlogComponent } from './tat-ca-blog/tat-ca-blog.component';
 import { ChiTietBlogComponent } from './chi-tiet-blog/chi-tiet-blog.component';
 import { TheoDoiDonHangComponent } from './theo-doi-don-hang/theo-doi-don-hang.component';
+import { TacGiaComponent } from './tac-gia/tac-gia.component';
+import { ChiTietTacGiaComponent } from './chi-tiet-tac-gia/chi-tiet-tac-gia.component';
+
 export const routes: Routes = [
     { path: '', redirectTo: '/trang-chu', pathMatch: 'full' },
   { path: 'trang-chu', component: TrangChuComponent },
@@ -12,5 +15,7 @@ export const routes: Routes = [
   { path: 'tat-ca-truyen', component: TatCaTruyenComponent },
   {path: 'tat-ca-blog', component: TatCaBlogComponent},
   {path: 'chi-tiet-blog', component: ChiTietBlogComponent},
-  { path: 'theo-doi-don-hang', component: TheoDoiDonHangComponent }
+  { path: 'theo-doi-don-hang', component: TheoDoiDonHangComponent },
+  {path: 'tac-gia', component: TacGiaComponent},
+  {path: 'chi-tiet-tac-gia/:id', component: ChiTietTacGiaComponent},
 ];
